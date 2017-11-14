bf2723c085b6f327f2efb09ccc5429894c612bcb
mnlmartinez <mnlmartinez68@gmail.com>
2017-11-14 23:29:35 +0100

    update for views

--- src/main/java/es/boostreator/BoostreatorApp.java
+++ src/main/java/es/boostreator/BoostreatorApp.java
@@ -5,27 +5,31 @@ import es.boostreator.domain.model.enums.Brand;
 import es.boostreator.domain.model.enums.Site;
 import es.boostreator.domain.service.CoffeeMachineService;
 import es.boostreator.util.AppLogger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.net.URL;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class BoostreatorApp {
+
+public class BoostreatorApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        String resourcePath = "/view/SearchView.fxml";
+        URL location = getClass().getResource(resourcePath);
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
 
     public static void main(String[] args) {
         AppLogger.debugMode();
-
-        CoffeeMachineService coffeeMachineService = new CoffeeMachineService();
-
-        String product = "Cafetera";
-        List<Site> sites = new ArrayList<>();
-        List<Brand> brands = new ArrayList<>();
-
-        sites.add(Site.ELCORTEINGLES);
-        sites.add(Site.FNAC);
-        brands.add(Brand.DELONGHI);
-        //brands.add(Brand.PHILIPS);
-
-        coffeeMachineService.getProductList(product, brands, sites, 20).forEach(System.out::println);
+        launch(args);
     }
 }
--- src/main/java/es/boostreator/view/SearchViewController.java
+++ src/main/java/es/boostreator/view/SearchViewController.java
@@ -1,6 +1,131 @@
 package es.boostreator.view;
 
 
-public class SearchViewController {
+import es.boostreator.domain.model.Product;
+import es.boostreator.domain.model.enums.Brand;
+import es.boostreator.domain.model.enums.Site;
+import es.boostreator.domain.service.CoffeeMachineService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import com.jfoenix.controls.*;
+import javafx.scene.text.Text;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class SearchViewController{
+
+    @FXML
+    private JFXSpinner loadCircle;
+
+    @FXML
+    private Text loadText;
+
+    @FXML
+    private JFXTextField searchBox;
+
+    @FXML
+    private JFXComboBox<String> categoriesDrawer;
+
+    @FXML
+    private JFXButton applyCategores;
+
+    @FXML
+    private JFXTextField searchBarInput;
+
+    @FXML
+    private JFXComboBox<String> brandDrawer;
+
+    @FXML
+    private JFXToggleButton fnacSwitch;
+
+    @FXML
+    private JFXToggleButton ElCorteInglesSwitch;
+
+
+    @FXML
+    private JFXListView<Product> resultsListView;
+
+    @FXML
+    public void applyBrandButton(ActionEvent actionEvent) {
+        //APPLY BRAND CHOICE
+    }
+
+    @FXML
+    public void applyCategoriesButton(ActionEvent actionEvent){
+        //APPLY CATEGORIES CHOICE
+    }
+
+    @FXML
+    public void searchBarInput(ActionEvent actionEvent){
+        //ON TEXT CHANGE
+        searchParameter = "cafetera " + searchBarInput.getText();
+    }
+
+    @FXML
+    public void initializeCategoriesDrawer(ActionEvent actionEvent){
+        ArrayList<String> categories = new ArrayList<>();
+        categories.add("Drop");
+        categories.add("Multi-function");
+        categories.add("Other");
+        categoriesDrawer.getItems().addAll();
+    }
+
+    @FXML
+    public void initializeBrandsDrawer(ActionEvent actionEvent){
+        ArrayList<String> brands = new ArrayList<>();
+        brands.add("Phillips");
+        brands.add("Nespresso");
+        brandDrawer.getItems().addAll(brands);
+    }
+
+
+    //FNAC SWITCH
+    public boolean fnacSwitchState = false;
+    //ELCORTEINGLES SWITCH
+    public boolean elCorteInglesSwitchState = false;
+
+    public List<Site> sites = new ArrayList<>();
+    public List<Brand> brands = new ArrayList<>();
+
+    //search parameter
+    public String searchParameter = "cafetera";
+
+    //Instantiate coffeeMachineService
+    CoffeeMachineService service = new CoffeeMachineService();
+
+    @FXML
+    public void findProducts(ActionEvent actionEvent){
+        System.out.println("heyyy");
+        findProducts();
+    }
+
+
+    public void findProducts(){
+        //configureParameters();
+        sites.add(Site.FNAC);
+
+        System.out.println("passed configure parameters");
+        List<Product> res = service.getProductList(searchParameter, brands, sites, 20);
+        resultsListView.getItems().addAll(res);
+        System.out.println(res);
+        loadCircle.setVisible(false);
+        loadText.setText("");
+    }
+
+    @FXML
+    public void changeFnacSwitch(ActionEvent actionEvent){ fnacSwitchState = !fnacSwitchState; }
+
+    @FXML
+    public void changeElCorteInglesSwitch(ActionEvent actionEvent){ elCorteInglesSwitchState = !elCorteInglesSwitchState; }
+
+
+    public void configureParameters(){
+        if(!fnacSwitchState && !elCorteInglesSwitchState) sites.add(Site.FNAC); sites.add(Site.ELCORTEINGLES);
+        if(elCorteInglesSwitchState) sites.add(Site.ELCORTEINGLES);
+        if(fnacSwitchState) sites.add(Site.FNAC);
+    }
 }
+
+
+
