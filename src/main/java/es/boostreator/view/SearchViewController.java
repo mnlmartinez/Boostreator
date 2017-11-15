0cf9474156289285362f026bdbd1ae1f11484c67
mnlmartinez <mnlmartinez68@gmail.com>
2017-11-15 14:09:05 +0100

    redesigns and finishing functionality

--- src/main/java/es/boostreator/view/SearchViewController.java
+++ src/main/java/es/boostreator/view/SearchViewController.java
@@ -1,9 +1,7 @@
 package es.boostreator.view;
 
 
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXSpinner;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
 import es.boostreator.domain.model.Product;
 import es.boostreator.domain.model.enums.Brand;
 import es.boostreator.domain.model.enums.Site;
@@ -12,10 +10,16 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -39,7 +43,7 @@ public class SearchViewController implements Initializable {
     private JFXTextField searchBarInput;
 
     @FXML
-    private JFXComboBox categoriesDrawer;
+    private JFXComboBox<String> categoriesDrawer;
 
     @FXML
     private JFXComboBox<Brand> brandsDrawer;
@@ -54,38 +58,51 @@ public class SearchViewController implements Initializable {
     private TableColumn modelColumn;
 
     @FXML
+    private TableColumn<Product, String> typeColumn;
+
+    @FXML
     private TableColumn<Product, String> priceECIColumn;
 
     @FXML
     private TableColumn<Product, String> priceFNACColumn;
 
+    @FXML
+    private JFXSlider sliderLimit;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         this.initializeBrandsDrawer();
-    }
+        this.initializeCategoriesDrawer();
 
-    @FXML
-    public void applyBrandButton(ActionEvent actionEvent) {
-        //APPLY BRAND CHOICE
-        //searchParameter+= " " +
-        //System.out.println(brandDrawer.getSelectionModel().selectedItemProperty().toString());
+        resultsListView.setRowFactory(tv -> {
+            TableRow<Product> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY
+                        && event.getClickCount() == 2) {
 
-    }
-
-    @FXML
-    public void applyCategoriesButton(ActionEvent actionEvent) {
-        //APPLY CATEGORIES CHOICE
-    }
-
-    @FXML
-    public void searchBarInput(ActionEvent actionEvent) {
-        //ON TEXT CHANGE
-        searchParameter = "cafetera " + searchBarInput.getText();
+                    Product clickedRow = row.getItem();
+                    System.out.println(clickedRow.toString());
+                    showDialog(clickedRow);
+                }
+            });
+            return row;
+        });
     }
 
 
     public void initializeBrandsDrawer() {
         brandsDrawer.getItems().addAll(Arrays.asList(Brand.values()));
+    }
+
+    public void initializeCategoriesDrawer() {
+        categoriesDrawer.getItems().addAll(
+                "Automática",
+                "Manual",
+                "Espresso",
+                "Italiana",
+                "Goteo",
+                "Capsulas"
+        );
     }
 
 
@@ -98,9 +115,6 @@ public class SearchViewController implements Initializable {
     public List<Site> sites = new ArrayList<>();
     public List<Brand> brands = new ArrayList<>();
 
-    //search parameter
-    public String searchParameter = "cafetera";
-
 
     //Instantiate coffeeMachineService
     CoffeeMachineService service = new CoffeeMachineService();
@@ -110,7 +124,12 @@ public class SearchViewController implements Initializable {
         findProducts();
         brandColumn.setCellValueFactory(new PropertyValueFactory("brand"));
         modelColumn.setCellValueFactory(new PropertyValueFactory("model"));
+
+
         try {
+            typeColumn.setCellValueFactory((c -> new SimpleStringProperty(
+                    c.getValue().toStringTypes().toLowerCase())));
+
             priceECIColumn.setCellValueFactory((c -> new SimpleStringProperty(
                     !c.getValue().getPrice().toString().contains("ELCORTEINGLES") ? "None" : c.getValue().toStringPrice(Site.ELCORTEINGLES))));
 
@@ -125,12 +144,10 @@ public class SearchViewController implements Initializable {
 
     public void findProducts() {
         configureParameters();
-        resultsListView.getItems().clear();//.removeAll();
-
-        System.out.println("passed configure parameters");
-        List<Product> res = service.getProductList(searchParameter, brands, sites, 20);
+        resultsListView.getItems().clear();
+        String selected = categoriesDrawer.getSelectionModel().getSelectedItem();
+        List<Product> res = service.getProductList(selected == null ? "cafetera" : "cafetera " + selected, brands, sites, (int) sliderLimit.getValue());
         resultsListView.getItems().addAll(res);
-        System.out.println(res);
     }
 
     @FXML
@@ -153,6 +170,36 @@ public class SearchViewController implements Initializable {
         if (elCorteInglesSwitchState) sites.add(Site.ELCORTEINGLES);
         if (fnacSwitchState) sites.add(Site.FNAC);
     }
+
+    public void showDialog(Product p) {
+
+
+        Stage dialog1 = new Stage();
+        dialog1.initStyle(StageStyle.UTILITY);
+        Text title = new Text("Product View");
+        title.setY(20.0);
+        title.setX(20.0);
+        Text brand = new Text("Brand: " + p.getBrand().name());
+        brand.setY(60.0);
+        brand.setX(20.0);
+        Text model = new Text("Model: " + p.getModel().toString());
+        model.setY(80.0);
+        model.setX(20.0);
+        Text types = new Text("Types: " + p.toStringTypes());
+        types.setY(100.0);
+        types.setX(20.0);
+        Text priceECI = new Text("Price EL CORTE INGLES: " + p.getPrice().get(Site.ELCORTEINGLES) == null ? "Not Found" : ("Price 'El Corte Ingles': " + p.getPrice().get(Site.ELCORTEINGLES)));
+        priceECI.setY(120.0);
+        priceECI.setX(20.0);
+        Text priceFNAC = new Text("Price FNAC: " + p.getPrice().get(Site.FNAC) == null ? "Not Found" : ("Price FNAC: " + p.getPrice().get(Site.FNAC)));
+        priceFNAC.setY(140.0);
+        priceFNAC.setX(20.0);
+        Scene scene = new Scene(new Group(title, brand, model, types, priceECI, priceFNAC), 550, 150);
+        dialog1.setScene(scene);
+        dialog1.show();
+    }
+
+
 }
 
 
